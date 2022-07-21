c81500fb72f71119cbd5d3ee4dda5bc207effb38
Sam-hw <109711096+Sam-hw@users.noreply.github.com>
2022-07-21 16:06:39 +0800

    Update iris-ml-app.py

--- iris-ml-app.py
+++ iris-ml-app.py
@@ -45,6 +45,61 @@ prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
 st.write(['Iris-setosa','Iris-versicolor','Iris-virginica'])
+
+import json
+from prettytable import PrettyTable
+
+value = ['''
+       {
+           "_id": {
+               "$Col1": "XXXXXXX2443"
+           },
+           "col2": false,
+           "col3": "359335050111111",
+           "startedAt": {
+               "$date": 1633309625000
+           },
+           "endedAt": {
+               "$date": 1633310213000
+           },
+           "col4": "YYYYYYYYYYYYYYYYYY",
+           "created_at": {
+               "$date": 1633310846935
+           },
+           "updated_at": {
+               "$date": 1633310846935
+           },
+           "__v": 0
+       }''']
+
+dictionary = json.loads(value[0])
+headers = []
+values = []
+for key in dictionary:
+    head = key
+    value = ""
+    if type(dictionary[key]) == type({}):
+        for key2 in dictionary[key]:
+            head += "/" + key2
+            value = dictionary[key][key2]
+            headers.append(head)
+            values.append(value)
+
+    else:
+        value = dictionary[key]
+        headers.append(head)
+        values.append(value)
+
+print(headers)
+print(values)
+myTable = PrettyTable(headers)
+
+myTable.add_row(values)
+print(myTable)
+
+
+
+
 st.subheader('Prediction')
 st.write(prediction)
 
