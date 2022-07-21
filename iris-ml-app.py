ac8bfacd65ab92d00d04ab5f1964201b43a578bb
Sam-hw <109711096+Sam-hw@users.noreply.github.com>
2022-07-21 15:36:18 +0800

    Update iris-ml-app.py

--- iris-ml-app.py
+++ iris-ml-app.py
@@ -32,8 +32,10 @@ st.write(df)
 #iris = datasets.load_iris()
 #X = iris.data
 
-X = pd.read_csv('https://raw.githubusercontent.com/ismailbahrudin/iris-july/main/IRIS.csv')
+iris = pd.read_csv('https://raw.githubusercontent.com/ismailbahrudin/iris-july/main/IRIS.csv')
+X = iris.drop('species',axis = 1)
 Y = iris['species']
+
 
 clf = RandomForestClassifier()
 clf.fit(X, Y)
